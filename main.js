1d22243236f161ca9b8f2bbdacd36cce1691170a
Daniel <danieltorreshomemfernande@gmail.com>
2023-10-26 21:50:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let friends = ['Alan', 'Mark', 'Julia', 'Bella', 1]
+console.log(friends)
+
+friends [1] = 'Anna' //Replace
+friends [4] = 'Maverick' //Add itens
+friends [5] = 1
+
+console.log(friends)
+console.log(friends[1])
